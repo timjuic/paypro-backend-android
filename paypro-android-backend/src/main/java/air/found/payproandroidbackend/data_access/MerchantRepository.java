e763ee7837a541ee376be87851fc23c70319c441
timjuic <tim.juic@gmail.com>
2023-11-23 09:37:51 +0100

    removed unused code

--- paypro-android-backend/src/main/java/air/found/payproandroidbackend/data_access/MerchantRepository.java
+++ paypro-android-backend/src/main/java/air/found/payproandroidbackend/data_access/MerchantRepository.java
@@ -1,12 +1,5 @@
 package air.found.payproandroidbackend.data_access;
 
-import air.found.payproandroidbackend.core.MerchantCardBrands;
-import air.found.payproandroidbackend.core.MerchantCardBrandsPK;
-
 public class MerchantRepository {
-    public void testMethod() {
-        MerchantCardBrands test = new MerchantCardBrands();
-
-    }
 
 }
