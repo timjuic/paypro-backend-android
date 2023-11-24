871433c3f11a3ea588b64ed6099d5c9ec09a4873
timjuic <tim.juic@gmail.com>
2023-11-24 18:12:19 +0100

    change api response to responseBody class.
    Add /api  to all endpoint paths

--- paypro-android-backend/src/main/java/air/found/payproandroidbackend/endpoints/controllers/MerchantController.java
+++ paypro-android-backend/src/main/java/air/found/payproandroidbackend/endpoints/controllers/MerchantController.java
@@ -1,15 +1,24 @@
 package air.found.payproandroidbackend.endpoints.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import air.found.payproandroidbackend.core.models.Merchants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import air.found.payproandroidbackend.core.network.ResponseBody;
+
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/merchant")
 public class MerchantController {
-    @GetMapping
-    public String getMerchant() {
-        return "test";
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseBody<List<Merchants>>> getMerchant(@PathVariable Long id) {
+
+        ResponseBody<List<Merchants>> response = (ResponseBody<List<Merchants>>) ResponseBody.error("API not implemented", 999, "ERR_NOT_IMPLEMENTED");
+        return new ResponseEntity<>(response, HttpStatus.NOT_IMPLEMENTED);
     }
+
+
 }
