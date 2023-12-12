b333fedcf45989e403dc12711e0189754479e277
aantonic20 <aantonic20@foi.hr>
2023-12-12 19:39:06 +0100

    Refactored the TerminalController and TerminalService and adjusted the endpoint responses in TerminalController.

--- paypro-android-backend/src/main/java/air/found/payproandroidbackend/business_logic/TerminalService.java
+++ paypro-android-backend/src/main/java/air/found/payproandroidbackend/business_logic/TerminalService.java
@@ -6,23 +6,22 @@ import air.found.payproandroidbackend.core.models.Merchant;
 import air.found.payproandroidbackend.core.models.Terminal;
 import air.found.payproandroidbackend.data_access.persistence.MerchantRepository;
 import air.found.payproandroidbackend.data_access.persistence.TerminalRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
+@RequiredArgsConstructor
 public class TerminalService {
+    private static final Pattern TERMINAL_KEY_PATTERN = Pattern.compile("[a-zA-Z0-9\\s$&'m@_-]{2,20}");
     private final TerminalRepository terminalRepository;
     private final MerchantRepository merchantRepository;
-    @Autowired
-    public TerminalService(TerminalRepository terminalRepository, MerchantRepository merchantRepository) {
-        this.terminalRepository = terminalRepository;
-        this.merchantRepository = merchantRepository;
-    }
 
-    public ServiceResult<Boolean> addTerminalToMerchant(Integer merchantId, Terminal terminal) {
+    public ServiceResult<Void> addTerminalToMerchant(Integer merchantId, Terminal terminal) {
         if (!isValidTerminalKey(terminal.getTerminalKey())) {
             return ServiceResult.failure(ApiError.ERR_INVALID_TERMINAL_KEY);
         }
@@ -31,56 +30,33 @@ public class TerminalService {
             return ServiceResult.failure(ApiError.ERR_TERMINAL_ALREADY_EXISTS);
         }
 
-        Optional<Merchant> optionalMerchant = merchantRepository.findById(merchantId);
-
-        if (optionalMerchant.isEmpty()) {
-            return ServiceResult.failure(ApiError.ERR_MERCHANT_NOT_FOUND);
-        }
-
-        Merchant merchant = optionalMerchant.get();
-        terminal.setMerchant(merchant);
-        terminalRepository.save(terminal);
-
-        return ServiceResult.success();
+         return merchantRepository.findById(merchantId)
+                 .map(merchant -> {
+                     terminal.setMerchant(merchant);
+                     terminalRepository.save(terminal);
+                     return ServiceResult.<Void>success();
+                 })
+                 .orElse(ServiceResult.failure(ApiError.ERR_MERCHANT_NOT_FOUND));
     }
 
 
     public ServiceResult<List<Terminal>> getTerminalsForMerchant(Integer merchantId) {
-        Optional<Merchant> optionalMerchant = merchantRepository.findById(merchantId);
-
-        if (optionalMerchant.isEmpty()) {
-            return ServiceResult.failure(ApiError.ERR_MERCHANT_NOT_FOUND);
-        }
-
-        List<Terminal> terminals = air.found.payproandroidbackend.data_access.manual.TerminalRepository.findByMerchantId(merchantId);
-        return ServiceResult.success(terminals);
+        return merchantRepository.findById(merchantId)
+                .map(merchant -> ServiceResult.success(air.found.payproandroidbackend.data_access.manual.TerminalRepository.findByMerchantId(merchantId)))
+                .orElse(ServiceResult.failure(ApiError.ERR_MERCHANT_NOT_FOUND));
     }
 
 
-    public ServiceResult<Boolean> deleteTerminal(Integer terminalId) {
-        Optional<Terminal> terminalOptional = terminalRepository.findById(terminalId);
-
-        if (terminalOptional.isEmpty()) {
-            return ServiceResult.failure(ApiError.ERR_TERMINAL_NOT_FOUND);
-        }
-
-        Terminal terminal = terminalOptional.get();
-        terminalRepository.delete(terminal);
-
-        return ServiceResult.success(null);
+    public ServiceResult<Void> deleteTerminal(Integer terminalId) {
+        return terminalRepository.findById(terminalId)
+                .map(terminal -> {
+                    terminalRepository.delete(terminal);
+                    return ServiceResult.<Void>success();
+                })
+                .orElse(ServiceResult.failure(ApiError.ERR_TERMINAL_NOT_FOUND));
     }
 
     private boolean isValidTerminalKey(String terminalKey) {
-        if (terminalKey.length() < 2 || terminalKey.length() > 20) {
-            return false;
-        }
-
-        terminalKey = terminalKey.trim();
-
-        if (!terminalKey.matches("[a-zA-Z0-9\\s$&'m@_-]+")) {
-            return false;
-        }
-
-        return true;
+        return TERMINAL_KEY_PATTERN.matcher(terminalKey.trim()).matches();
     }
 }
