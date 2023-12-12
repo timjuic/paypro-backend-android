59c8067c0eab9fe7043a89e662139e58469ef18d
aantonic20 <aantonic20@foi.hr>
2023-12-12 20:25:25 +0100

    Refactored the UserController and UserService and adjusted the endpoint responses in UserController.

--- paypro-android-backend/src/main/java/air/found/payproandroidbackend/endpoints/controllers/UserController.java
+++ paypro-android-backend/src/main/java/air/found/payproandroidbackend/endpoints/controllers/UserController.java
@@ -8,6 +8,7 @@ import air.found.payproandroidbackend.core.models.JwtTokenInfo;
 import air.found.payproandroidbackend.core.models.UserAccount;
 import air.found.payproandroidbackend.core.network.ApiResponseBuilder;
 import air.found.payproandroidbackend.core.network.ResponseBody;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,35 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
     private final TokenService tokenService;
 
-    @Autowired
-    public UserController(UserService userService, TokenService tokenService) { this.userService = userService; this.tokenService = tokenService; }
-
     @PostMapping("/register")
-    public ResponseEntity<ResponseBody<Object>> registration(@RequestBody UserAccount userAccount) {
-        ServiceResult<Boolean> result = userService.registerUser(userAccount);
-
-        if (result.isSuccess()) {
-            return ApiResponseBuilder.buildSuccessResponse(null, "You have been successfully registered!");
-        }
-
-        ApiError apiError = result.getApiError();
-        return ApiResponseBuilder.buildErrorResponse(HttpStatus.BAD_REQUEST, apiError.getErrorMessage(), apiError.getErrorCode(), apiError.getErrorName());
+    public ResponseEntity<ResponseBody<Void>> registration(@RequestBody UserAccount userAccount) {
+        ServiceResult<Void> result = userService.registerUser(userAccount);
+        return respond(result, "You have been successfully registered");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseBody<Object>> login(@RequestBody UserAccount userAccount) {
+    public ResponseEntity<ResponseBody<JwtTokenInfo>> login(@RequestBody UserAccount userAccount) {
         ServiceResult<UserAccount> result = userService.loginUser(userAccount);
-
         if (result.isSuccess()) {
             JwtTokenInfo token = tokenService.getJwtToken(result.getData());
             return ApiResponseBuilder.buildSuccessResponse(token, "You have been successfully logged in!");
         }
-        else {
+        ApiError apiError = result.getApiError();
+        return ApiResponseBuilder.buildErrorResponse(HttpStatus.BAD_REQUEST, apiError.getErrorMessage(), apiError.getErrorCode(), apiError.getErrorName());
+    }
+
+    private <T> ResponseEntity<ResponseBody<T>> respond(ServiceResult<T> result, String successMessage) {
+        if (result.isSuccess()) {
+            return ApiResponseBuilder.buildSuccessResponse(result.getData(), successMessage);
+        } else {
             ApiError apiError = result.getApiError();
             return ApiResponseBuilder.buildErrorResponse(HttpStatus.BAD_REQUEST, apiError.getErrorMessage(), apiError.getErrorCode(), apiError.getErrorName());
         }
