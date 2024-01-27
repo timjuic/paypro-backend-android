b4614ee8ce97161dcd9a3834e92928bf1f9ec930
timjuic <tim.juic@gmail.com>
2024-01-27 12:41:48 +0100

    Loading secret variables

--- paypro-android-backend/src/main/java/air/found/payproandroidbackend/PayproAndroidBackendApplication.java
+++ paypro-android-backend/src/main/java/air/found/payproandroidbackend/PayproAndroidBackendApplication.java
@@ -1,11 +1,12 @@
 package air.found.payproandroidbackend;
 
+import air.found.payproandroidbackend.config.SecretsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication()
+@EnableConfigurationProperties(SecretsProperties.class)
 public class PayproAndroidBackendApplication {
 
 	public static void main(String[] args) {
